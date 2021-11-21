2de6732d82729c03af9accd954eca2a1e90f49e1
keshavdulal <keshav.dulal@gmail.com>
2021-11-21 23:36:40 +0545

    first commit

---
+++ app/src/main/java/com/example/mapd711_a4/MainActivity.kt
@@ -0,0 +1,13 @@
+ls
+ls
+package com.example.mapd711_a4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
