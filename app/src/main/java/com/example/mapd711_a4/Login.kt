2665153cda023d1e200d5e2e38af32614ce696f5
keshavdulal <keshav.dulal@gmail.com>
2021-11-22 00:26:31 +0545

    setup(Login): layout

--- app/src/main/java/com/example/mapd711_a4/Login.kt
+++ app/src/main/java/com/example/mapd711_a4/Login.kt
@@ -1,13 +1,11 @@
-ls
-ls
 package com.example.mapd711_a4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
