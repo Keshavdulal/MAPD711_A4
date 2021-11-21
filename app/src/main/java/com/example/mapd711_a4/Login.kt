5657e2e359c3ad6cb33a44d7f0a4a76d403f8201
keshavdulal <keshav.dulal@gmail.com>
2021-11-22 00:35:35 +0545

    add(Screens): Register & Dashboard

--- app/src/main/java/com/example/mapd711_a4/Login.kt
+++ app/src/main/java/com/example/mapd711_a4/Login.kt
@@ -1,11 +1,26 @@
 package com.example.mapd711_a4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginBtn = findViewById<Button>(R.id.buttonLogin)
+        val registerBtn = findViewById<Button>(R.id.buttonRegister)
+
+        loginBtn.setOnClickListener {
+            val intent = Intent(this@Login, Dashboard::class.java)
+            startActivity(intent)
+        }
+
+        registerBtn.setOnClickListener {
+            val intent = Intent(this@Login, Registration::class.java)
+            startActivity(intent)
+        }
     }
 }
