a104dfa57fc51c13d9953ce9d9f5bbc93029796b
Aishwarya Shrestha <aishwaryashrestha6094@gmail.com>
2021-11-22 01:34:53 -0500

    Added booking details

---
+++ app/src/main/java/com/example/mapd711_a4/CruiseDetail.kt
@@ -0,0 +1,11 @@
+package com.example.mapd711_a4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CruiseDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cruise_detail)
+    }
+}
